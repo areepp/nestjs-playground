9438afda6cb0e59403727561e8677d9821492c20
Arif Amijoyo <arifamjy@gmail.com>
2024-10-04 21:23:20 +0700

    feat: initialize docker compose and validate env vars

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './modules/users/users.module';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './utils/env.validation';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, ConfigModule.forRoot({ validate })],
   controllers: [AppController],
   providers: [AppService],
 })
---
+++ src/utils/env.validation.ts
@@ -0,0 +1,46 @@
+import { plainToInstance } from 'class-transformer';
+import { IsEmail, IsNumber, IsString, validateSync } from 'class-validator';
+
+enum Environment {
+  Development = 'development',
+  Production = 'production',
+  Test = 'test',
+  Provision = 'provision',
+}
+
+class EnvironmentVariables {
+  @IsString()
+  POSTGRES_USER: string;
+
+  @IsString()
+  POSTGRES_PASSWORD: string;
+
+  @IsString()
+  POSTGRES_DB: string;
+
+  @IsEmail()
+  PGADMIN_DEFAULT_EMAIL: string;
+
+  @IsString()
+  PGADMIN_DEFAULT_PASSWORD: string;
+
+  @IsString()
+  POSTGRES_HOST: string;
+
+  @IsNumber()
+  POSTGRES_PORT: number;
+}
+
+export function validate(config: Record<string, unknown>) {
+  const validatedConfig = plainToInstance(EnvironmentVariables, config, {
+    enableImplicitConversion: true,
+  });
+  const errors = validateSync(validatedConfig, {
+    skipMissingProperties: false,
+  });
+
+  if (errors.length > 0) {
+    throw new Error(errors.toString());
+  }
+  return validatedConfig;
+}

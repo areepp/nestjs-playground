29059fd5160aff77bd591f4f1ba0604c43253b51
Arif Amijoyo <arifamjy@gmail.com>
2024-10-18 20:55:00 +0700

    feat: bind login and register page with react-hook-form

--- apps/client/src/features/auth/components/page-login.tsx
+++ apps/client/src/features/auth/components/page-login.tsx
@@ -9,10 +9,35 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
 import Link from "next/link";
+import { z } from "zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+const schemaLogin = z.object({
+  email: z.string().email(),
+  password: z.string(),
+});
+
+type SchemaLogin = z.infer<typeof schemaLogin>;
 
 export default function LoginPage() {
+  const form = useForm<SchemaLogin>({
+    resolver: zodResolver(schemaLogin),
+  });
+
+  function onSubmit(values: SchemaLogin) {
+    console.log(values);
+  }
+
   return (
     <div className="my-auto mt-48 flex flex-col items-center justify-center">
       <Card className="w-[500px]">
@@ -38,17 +63,45 @@ export default function LoginPage() {
               </span>
             </div>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        placeholder="m@example.com"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </CardContent>
         <CardFooter className="flex-col gap-3">
-          <Button className="w-full">Log in</Button>
+          <Button className="w-full" onClick={form.handleSubmit(onSubmit)}>
+            Log in
+          </Button>
           <p className="self-end text-xs">
             Don&apos;t have an account?{" "}
             <Link href="/auth/register" className="underline">
--- apps/client/src/features/auth/components/page-register.tsx
+++ apps/client/src/features/auth/components/page-register.tsx
@@ -10,10 +10,43 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
-import { Label } from "@/components/ui/label";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+
+const schemaRegister = z
+  .object({
+    email: z.string().email(),
+    password: z
+      .string()
+      .min(7, { message: "Must be at least 7 characters long" })
+      .regex(/\d/, { message: "Must contain at least one number" }),
+    confirmPassword: z.string(),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    message: "Passwords do not match",
+    path: ["confirmPassword"],
+  });
+type SchemaRegister = z.infer<typeof schemaRegister>;
 
 export default function CreateAccountPage() {
+  const form = useForm<SchemaRegister>({
+    resolver: zodResolver(schemaRegister),
+  });
+
+  function onSubmit(values: SchemaRegister) {
+    console.log(values);
+  }
+
   return (
     <div className="my-auto mt-48 flex items-center justify-center">
       <Card className="w-[500px]">
@@ -42,21 +75,62 @@ export default function CreateAccountPage() {
               </span>
             </div>
           </div>
-          <div className="grid gap-2">
-            <Label htmlFor="email">Email</Label>
-            <Input id="email" type="email" placeholder="m@example.com" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password">Password</Label>
-            <Input id="password" type="password" />
-          </div>
-          <div className="grid gap-2">
-            <Label htmlFor="password-confirmation">Confirm your password</Label>
-            <Input id="password-confirmation" type="password" />
-          </div>
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)}>
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Email</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="email"
+                        placeholder="m@example.com"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="password"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Password</FormLabel>
+                    <FormControl>
+                      <Input {...field} type="password" />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={form.control}
+                name="confirmPassword"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Confirm your password</FormLabel>
+                    <FormControl>
+                      <Input
+                        {...field}
+                        type="password"
+                        placeholder="m@example.com"
+                      />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
         </CardContent>
         <CardFooter className="flex-col gap-3">
-          <Button className="w-full">Create account</Button>
+          <Button className="w-full" onClick={form.handleSubmit(onSubmit)}>
+            Create account
+          </Button>
           <p className="self-end text-xs">
             Already have an account?{" "}
             <Link href="/auth/login" className="underline">

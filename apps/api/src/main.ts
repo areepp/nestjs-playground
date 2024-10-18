6839e3571e67e083d4a2789e84d0fada62ff8f71
Arif Amijoyo <arifamjy@gmail.com>
2024-10-18 21:22:23 +0700

    feat: prefixing 'api'

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,6 +5,7 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
+  app.setGlobalPrefix('api');
   app.use(helmet());
   app.use(cookieParser());
   await app.listen(3000);

d788a62d03e4aa730aebbda08531159744345918
Arif Amijoyo <arifamjy@gmail.com>
2024-11-11 11:02:37 +0700

    feat: add logout button to auth page

--- apps/client/src/features/profile/components/page-profile/index.tsx
+++ apps/client/src/features/profile/components/page-profile/index.tsx
@@ -1,11 +1,14 @@
 "use client";
 
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { useLogout } from "@/features/auth/api/logout";
 import { EditProfileDialog } from "./edit-profile-dialog";
 import { useGetMyProfile } from "../../api/get-my-profile";
 
 export default function PageProfile() {
   const { data } = useGetMyProfile({});
+  const { mutate: logoutMutation } = useLogout({});
 
   return (
     <div className="h-full flex flex-col items-center justify-center gap-9">
@@ -18,6 +21,9 @@ export default function PageProfile() {
       />
       <h1 className="text-4xl font-bold">Hi, {data?.name}!</h1>
       <EditProfileDialog />
+      <Button variant="secondary" onClick={() => logoutMutation(undefined)}>
+        Log out
+      </Button>
     </div>
   );
 }
